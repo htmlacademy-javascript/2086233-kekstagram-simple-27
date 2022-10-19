2ac3e90097b30fbff67a395aae7e158e1c215756
Keks <keks@htmlacademy.ru>
2022-10-19 11:50:20 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+import {postsContent} from './data.js';
+
+console.log(postsContent);
+
+
+//Импорты других модулей, Вызовы общих функций, Настройка скриптов..
---
+++ 4/js/utils.js
@@ -0,0 +1,32 @@
+export {getRandomInteger, checkLength};
+/* Функция, возвр-я случайное целое число из переданного диапазона
+(с использованием ресурса - https://learn.javascript.ru/task/random-int-min-max)*/
+
+function getRandomInteger(firstNumber, secondNumber) {
+  if (firstNumber < 0 || secondNumber < 0) {
+    return NaN; }
+  else if (firstNumber > secondNumber) {
+    const swap = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = swap;
+  }
+  firstNumber = Math.ceil(firstNumber);
+  secondNumber = Math.floor(secondNumber);
+  return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+}
+
+/* Функция для проверки максимальной длины строки.
+(с использованием ресурса - https://learn.javascript.ru/task/truncate)*/
+
+const MIN_LENGTH = 20;
+const MAX_LENGTH = 140;
+const errMsg = 'Слишком короткий комментарий!';
+
+function checkLength(string) {
+  if (string.length < MIN_LENGTH) {
+    throw Error(errMsg);
+  }
+  return (string.length > MAX_LENGTH) ?
+    `${string.slice(0, MAX_LENGTH - 1)}…` : string;
+}
+
